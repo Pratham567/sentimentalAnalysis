4da5371df405bc8fa1375dfc8920bef3e30db181
nishantsikarwar <nsikarwar@ch.iitr.ac.in>
2020-09-15 16:58:59 +0530

    added the customises model for training the data

--- visualization/basicAnalysisPlot.py
+++ visualization/basicAnalysisPlot.py
@@ -1,4 +1,5 @@
-from basicAnalyseML import res
+# from basicAnalyseML import res
+from analysis import w
 import numpy as np
 import pandas as pd
 from pandas import DataFrame
@@ -6,15 +7,21 @@ from pandas import DataFrame
 import matplotlib.pyplot as plt
 
 # the Seaborn library
-import seaborn as sns
-sns.set()
+# import seaborn as sns
+# sns.set()
 
 # convert the classified Data into panda dataframes for plotting.
-df = pd.DataFrame()
-for elem in res:
-    df = df.append(elem['classifications'])
+# df = pd.DataFrame()
+# for elem in res:
+#     df = df.append(elem['classifications'])
 
-_, axes = plt.subplots(nrows=1, ncols=1, figsize=(12, 4))
-sns.countplot(x='tag_name',data=df, ax=axes)
-print(res)
-print(df)
+# _, axes = plt.subplots(nrows=1, ncols=1, figsize=(12, 4))
+# sns.countplot(x='tag_name',data=df, ax=axes)
+# print(res)
+# print(df)
+
+fig, ax1 = plt.subplots()
+ax1.bar(w.keys(), w.values(), color=(0.2, 0.4, 0.6, 0.6))
+fig.autofmt_xdate()
+plt.savefig('graph.png')
+plt.show()
